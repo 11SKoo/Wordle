ad05557c1ad30f959eefe6f6b05354af9c60ea26
11SKoo <kotobapu@gmail.com>
2025-06-13 05:05:39 +0900

    rm

--- src/test.py
+++
@@ -1,8 +0,0 @@
-# ファイルを読み込んで大文字に変換し、書き戻す
-with open("words.txt", "r", encoding="utf-8") as file:
-    content = file.read()
-
-content_upper = content.upper()
-
-with open("words.txt", "w", encoding="utf-8") as file:
-    file.write(content_upper)
